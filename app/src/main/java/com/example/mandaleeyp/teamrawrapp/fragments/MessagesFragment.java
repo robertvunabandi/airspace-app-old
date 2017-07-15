3e8ab232b648e754554279961dab8e5972f6c9b7
Ruben Dicker <rdicker@fb.com>
2017-07-14 17:15:20 -0700

    more message stuff that doesn't work

--- app/src/main/java/com/example/mandaleeyp/teamrawrapp/fragments/MessagesFragment.java
+++ app/src/main/java/com/example/mandaleeyp/teamrawrapp/fragments/MessagesFragment.java
@@ -1,14 +1,19 @@
 package com.example.mandaleeyp.teamrawrapp.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.mandaleeyp.teamrawrapp.MessageAdapter;
 import com.example.mandaleeyp.teamrawrapp.R;
+import com.example.mandaleeyp.teamrawrapp.models.Msg;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +23,24 @@ import com.example.mandaleeyp.teamrawrapp.R;
  * Use the {@link MessagesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
+
 public class MessagesFragment extends Fragment {
+
+    RecyclerView rvConversation;
+    ArrayList<Msg> messages;
+    MessageAdapter messageAdapter;
+
+    EditText etMessage;
+    Button btnSend;
+
+
+
+
+
+
+
+    /*
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -42,12 +64,15 @@ public class MessagesFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment MessagesFragment.
      */
+
+
+
     // TODO: Rename and change types and number of parameters
     public static MessagesFragment newInstance(String param1, String param2) {
         MessagesFragment fragment = new MessagesFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,10 +80,16 @@ public class MessagesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        setContentView(R.layout.fragment_messages);
+
+
+
+
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
