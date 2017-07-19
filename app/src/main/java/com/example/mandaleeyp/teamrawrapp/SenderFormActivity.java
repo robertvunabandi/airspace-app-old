0620039e417ef11964cd4a657df9ee988b2da122
Amanda Lee <mandaleeyp@MacBook-Pro-8.dhcp.thefacebook.com>
2017-07-19 15:17:49 -0700

    Added reciever form

--- app/src/main/java/com/example/mandaleeyp/teamrawrapp/SenderFormActivity.java
+++ app/src/main/java/com/example/mandaleeyp/teamrawrapp/SenderFormActivity.java
@@ -3,11 +3,11 @@ package com.example.mandaleeyp.teamrawrapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RequestFormActivity extends AppCompatActivity {
+public class SenderFormActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_request_form);
+        setContentView(R.layout.activity_sender_form);
     }
 }
